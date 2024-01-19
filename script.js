e7f53e6ce8cea6ebff8b99e6c185389ebafeeea2
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-20 02:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let arr=[];
+	let z=0;
+	let i=0
+	while(i<str.length-1){
+		let s="";
+	  i=z;
+		z=z+size;
+	    
+		for(let j=i;j<z;j++){
+			s=s+str.charAt(j);
+		}
+		arr.push(s);
+		
+	}
+	return arr;
 }
 
 // Do not change the code below
